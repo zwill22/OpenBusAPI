6bf915b10fb7e3064e8d15e5736ad92efe1a32cf
Zack Williams <73105122+zwill22@users.noreply.github.com>
2025-05-09 12:29:42 +0100

    Updated doc config with apikey

--- docs/conf.py
+++ docs/conf.py
@@ -11,10 +11,10 @@ sys.path.insert(0, os.path.abspath(".."))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'OpenBusAPI'
-copyright = '2025, Z M Williams'
-author = 'Z M Williams'
-release = '0.2.0'
+project = "OpenBusAPI"
+copyright = "2025, Z M Williams"
+author = "Z M Williams"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -24,21 +24,19 @@ extensions = [
     "sphinx.ext.autodoc",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
 
 html_context = {
     "display_github": True,
     "github_user": "zwill22",
     "github_repo": "OpenBusAPI",
     "conf_py_path": "/docs/",
-    "github_version": "main"
+    "github_version": "main",
 }
